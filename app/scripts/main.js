f4591e67a7643f20fe1fcd6ebab76c3e05883643
fishlegz <velvetwall@protonmail.com>
2017-11-28 02:49:15 +0000

    Modal functionality, redesign etc

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,42 @@
 jQuery(function($){
-   $('#modal1FormPhone').mask('+7 (999) 999-9999');
-   $('#modal2FormPhone').mask('+7 (999) 999-9999');
+   
+   $('#modalDownloadPhone').mask('+7 (999) 999-9999');
+   $('#modalOrderCallPhone').mask('+7 (999) 999-9999');
+   $('#modalTestDrivePhone').mask('+7 (999) 999-9999');
+   
+   var $modalDownload  = $('#ModalDownload'),
+       $modalThanks    = $('#ModalThanks'),
+       $modalTestDrive = $('#ModalTestDrive');
+   
+   // $('#modalDownloadForm').on('submit', function() {
+   //    if( $('#modalDownloadName').val() && $('#modalDownloadPhone').val() ) {
+   //       $modalDownload.modal('hide');
+   //       $('.modal-thanks').removeClass('not-show');
+   //       $('.modal-blurred').addClass('blur');
+   //       // $('#modalDownloadForm').submit();
+   //       setTimeout(function() {
+   //          $modalTestDrive.modal('show');
+   //       }, 350);
+   //    }
+   // });
+   
+   $('.thanks-close, .btn-video').on('click', function() {
+      $('.modal-thanks').addClass('not-show');
+      $('.modal-blurred').removeClass('blur');
+   });
+   
+   $("#modalDownloadForm").validate({
+      submitHandler: function(form) {
+         $modalDownload.modal('hide');
+         $('.modal-thanks').removeClass('not-show');
+         $('.modal-blurred').addClass('blur');
+         setTimeout(function() {
+            $modalTestDrive.modal('show');
+         }, 350);
+         // $(form).submit();
+      }
+   });
+   $('#modalOrderCallForm').validate();
+   $('#modalTestDriveForm').validate();
+   
 });
